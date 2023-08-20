8f7f2b18ddccbc0285177e8744dc94faf1752132
Stefano Consonni <stefanoconsonni@ymail.com>
2023-08-20 14:30:46 +0200

    add isLoading template

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import { useState } from "react";
+import { useFetch } from "./hooks/useFetch";
 import Tools from "./components/Tools/Tools";
 import { colors } from "./utils/colors";
 
 function App() {
   const [currentColor, setCurrentColor] = useState(colors.c1);
   const [user, setUser] = useState("");
+  const { data, isLoading, error } = useFetch("http://localhost:8000/");
 
+  console.log("error", error);
+  console.log("data", data);
   console.log("currentColor", currentColor);
 
   function SquareColorize(color) {
@@ -16,6 +20,14 @@ function App() {
 
   function handleCurrentUser(value) {
     setUser(value);
+  }
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <div className="loading-text">Loading...</div>
+      </div>
+    );
   }
 
   return (
