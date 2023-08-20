af9940effc9296b785bacfe997cc767499a6c116
Stefano Consonni <stefanoconsonni@ymail.com>
2023-08-20 22:20:00 +0200

    create renderGrid function

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useFetch } from "./hooks/useFetch";
 import { colors } from "./utils/colors";
 import { Tools, Square } from "./components";
@@ -7,11 +7,6 @@ function App() {
   const [currentColor, setCurrentColor] = useState(colors.c1);
   const [user, setUser] = useState("");
   const { data, isLoading, error } = useFetch("http://localhost:8000/");
-
-  console.log("error", error);
-  console.log("data", data);
-  console.log("currentColor", currentColor);
-  if (data) console.log("Object.entries(data)", Object.entries(data));
 
   function SquareColorize(color) {
     let r = document.querySelector(":root");
@@ -23,20 +18,30 @@ function App() {
     setUser(value);
   }
 
+  const renderGrid = useMemo(() => {
+    const squares = [];
+    for (let i = 0; i < 6400; i++) {
+      const x = parseInt(i / 80);
+      const y = parseInt(i % 80);
+      const key = `${x}-${y}`;
+      const squareData = (data || {})[key];
+
+      squares.push(<Square key={key} position={key} color={squareData?.color} />);
+    }
+    return squares;
+  }, [data]);
+
   return (
     <div className="app">
       <Tools user={user} handleCurrentUser={handleCurrentUser} setCurrentColor={SquareColorize} />
       <div className="board">
-        {isLoading && (
-          <div className="loading">
-            <div className="loading-text">Loading...</div>
-          </div>
-        )}
         <div className="canvas">
-          {data &&
-            Object.entries(data).map((position) => (
-              <Square key={position[0]} position={position[0]} color={position[1].color} />
-            ))}
+          {isLoading && (
+            <div className="loading">
+              <div className="loading-text">Loading...</div>
+            </div>
+          )}
+          {renderGrid}
         </div>
       </div>
     </div>
--- client/src/components/Square.js
+++ client/src/components/Square.js
@@ -1,7 +1,7 @@
 function Square({ position, color }) {
-  console.log("color", color);
-  console.log("position", position);
-  return <div className="square" position={position} style={{ backgroundColor: color }}></div>;
+  return (
+    <div className="square" position={position} style={{ backgroundColor: color || "white" }}></div>
+  );
 }
 
 export default Square;
