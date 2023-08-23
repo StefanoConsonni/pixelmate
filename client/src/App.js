f181399fe82c017d4acc26059cffa340ee0a3358
Stefano Consonni <stefanoconsonni@ymail.com>
2023-08-23 12:17:31 +0200

    setup canvas-api initial implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
-import { useCallback, useMemo } from "react";
+import { useCallback, useMemo, useRef, useEffect, useState } from "react";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import toast, { Toaster } from "react-hot-toast";
-import { Tools, Square, Loading, ErrorComponent } from "./components";
+import { Tools, Loading, ErrorComponent } from "./components";
 import { useFetch, useCooldown } from "./hooks";
 import { API, GRID_SIZE } from "./utils/constants";
 import { colors } from "./utils/colors";
 
 let currentUser = "";
 let currentColor = colors.c1;
+const scaleSize = 1.5;
 
 function App() {
   const { data, isLoading, error } = useFetch(API.MAIN_URL);
   const { canChangeColor, cooldown, startCooldown } = useCooldown();
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+  const activePixelRef = useRef(null);
+  const [overlayStyle, setOverlayStyle] = useState({ top: "0px", left: "0px" });
+  console.log("overlayStyle", overlayStyle);
 
   const updateSquare = useCallback((coordinate, color) => {
     // Get the current timestamp and store it in localStorage
@@ -66,16 +72,20 @@ function App() {
     (e, square) => {
       if (currentUser) {
         if (canChangeColor) {
-          updateSquare(square.coordinate, currentColor)
-            .then((resp) => {
-              square.color = currentColor;
-              square.user = currentUser;
-              startCooldown();
-              toast.success("Color successfully changed!");
-            })
-            .catch((err) => {
-              throw new Error(err);
-            });
+          // updateSquare(square.coordinate, currentColor)
+          //   .then((resp) => {
+          //     square.color = currentColor;
+          //     square.user = currentUser;
+          //     startCooldown();
+          //     toast.success("Color successfully changed!");
+          //   })
+          //   .catch((err) => {
+          //     throw new Error(err);
+          //   });
+          const clickedX = Math.floor(e.pageX / scaleSize);
+          const clickedY = Math.floor(e.pageY / scaleSize);
+          setOverlayStyle({ top: `${clickedY * scaleSize}px`, left: `${clickedX * scaleSize}px` });
+          activePixelRef.current = `${clickedX}-${clickedY}`;
         }
       } else {
         toast.error("Username missing!");
@@ -83,6 +93,27 @@ function App() {
     },
     [canChangeColor, startCooldown, updateSquare]
   );
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth * 2;
+    canvas.height = window.innerHeight * 2;
+    canvas.style.width = `${window.innerWidth}px`;
+    canvas.style.height = `${window.innerHeight}px`;
+
+    const context = canvas.getContext("2d");
+    // context.lineCap = "round";
+    // context.strokeStyle = "black";
+    // context.scale(2, 2);
+    context.lineWidth = 5;
+    contextRef.current = context;
+
+    squaresData.forEach((square) => {
+      const [x, y] = square.coordinate.split("-");
+      context.fillRect(x, y, 2, 2);
+      context.fillStyle = square.color;
+    });
+  }, [squaresData]);
 
   return (
     <div className="app">
@@ -94,13 +125,22 @@ function App() {
         <TransformWrapper initialScale={1} panning={{ disabled: true }} pinch={{ disabled: true }}>
           <TransformComponent>
             <div className="board">
-              <div className="canvas">
+              <canvas className="canvas" ref={canvasRef} onClick={handleSquareClick}>
                 {isLoading && <Loading />}
                 {error && <ErrorComponent />}
-                {(squaresData || []).map((square) => (
-                  <Square key={square.coordinate} square={square} handleClick={handleSquareClick} />
-                ))}
-              </div>
+                {/* {(squaresData || []).map(
+                  (square) =>   (
+                    <Square key={square.coordinate} square={square} handleClick={handleSquareClick} />
+
+
+
+                  )
+
+                )} */}
+              </canvas>
+              {activePixelRef && (
+                <div id="overlay-id" className="overlay-style" style={overlayStyle}></div>
+              )}
             </div>
           </TransformComponent>
         </TransformWrapper>
